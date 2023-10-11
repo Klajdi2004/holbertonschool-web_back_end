97ac52d77e7097c3253c531b0b5a9bfad2ff2d32
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-10-11 18:18:19 +0200

    Update 0-promise.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,12 +1,11 @@
 // 0-promise.js
 
-const getResponseFromAPI = () => {
-    return new Promise((resolve, reject) => {
-      // Your API call logic here
-      
-      // Use resolve(value) to resolve the Promise with a value
-      // Use reject(error) to reject the Promise with an error
-    });
-};
+const getResponseFromAPI = () =>
+  new Promise((resolve, reject) => {
+    // Your API call logic here
+    
+    // Use resolve(value) to resolve the Promise with a value
+    // Use reject(error) to reject the Promise with an error
+  });
   
 export default getResponseFromAPI;
