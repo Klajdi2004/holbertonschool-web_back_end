c13827033429473c36d9a318a5580273f79624be
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-10-14 15:01:08 +0200

    fixxed lint trailing spaces

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -12,7 +12,7 @@ export class HolbertonClass {
     return this._location;
   }
 }
-  
+
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
@@ -32,7 +32,7 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
-  
+
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
